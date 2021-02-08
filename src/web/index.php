8be252f3670e407037dc6af97104c588e46c109b
JorgeGV98 <61948379+jorgegv98@users.noreply.github.com>
2021-02-08 12:29:06 +0100

    Update Index

--- src/web/index.php
+++ src/web/index.php
@@ -32,7 +32,7 @@ echo ' echo "<head>";
         echo "<link rel="stylesheet" type="text/css" href="./css/main.css" media="screen" />";
     echo "</head>";';
 
-    echo '<h1 class=g--background-color-1>JORGE GARCIA</h1>';
+    echo '<h1 class=g--background-rojo-1>JORGE GARCIA</h1>';
 
 var_dump($result); 
 } catch (Exception $e) {
